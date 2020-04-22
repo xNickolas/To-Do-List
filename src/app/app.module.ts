07f41e211382e989ad2ca3c2dbbb3bbed015a701
xNickolas <nickolsctt@gmail.com>
2020-04-22 14:25:45 -0300

    New task-cards component, created css style and new input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +11,9 @@ import { HeaderComponent } from './components/template/header/header.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
 import { MainViewComponent } from './components/main-view/main-view.component';
 import { NavBarComponent } from './components/template/menu/nav-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { TaskCardsComponent } from './components/task-cards/task-cards.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +21,18 @@ import { NavBarComponent } from './components/template/menu/nav-bar.component';
     HeaderComponent,
     AddTaskComponent,
     MainViewComponent,
-    NavBarComponent
+    NavBarComponent,
+    TaskCardsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/task-cards/task-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-cards',
+  templateUrl: './task-cards.component.html',
+  styleUrls: ['./task-cards.component.css']
+})
+export class TaskCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
